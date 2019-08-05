0441baf6152d0266e67f5058f3a6bbb2f03d1f57
maira <mairabaptista@hotmail.com>
2019-08-05 18:31:26 -0300

    Learning cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\Users\ITX-GAMER\Documents\RustBook\hello_cargo\target\debug\hello_cargo.pdb: D:\Users\ITX-GAMER\Documents\RustBook\hello_cargo\src\main.rs
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-d2b920ddc232ac03.d
@@ -0,0 +1,5 @@
+d:\Users\ITX-GAMER\Documents\RustBook\hello_cargo\target\rls\debug\deps\hello_cargo-d2b920ddc232ac03.rmeta: src\main.rs
+
+d:\Users\ITX-GAMER\Documents\RustBook\hello_cargo\target\rls\debug\deps\hello_cargo-d2b920ddc232ac03.d: src\main.rs
+
+src\main.rs:
